386d5794a26d69fbfcaf036b96b5fdf1fbbc6c16
sreelakshmimukkara <97490442+sreelakshmimukkara@users.noreply.github.com>
2022-03-15 15:36:24 +0530

    Add files via upload

---
+++ helloworldouput.py
@@ -0,0 +1,15 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/CSELAB/AppData/Local/Programs/Python/Python39/helloworld.py
+Traceback (most recent call last):
+  File "C:/Users/CSELAB/AppData/Local/Programs/Python/Python39/helloworld.py", line 1, in <module>
+    print(helloworld)
+NameError: name 'helloworld' is not defined
+>>> 
+= RESTART: C:/Users/CSELAB/AppData/Local/Programs/Python/Python39/helloworld.py
+helloworld
+>>> 
+========================== RESTART: D:/helloworld.py =========================
+helloworld!
+>>> 
